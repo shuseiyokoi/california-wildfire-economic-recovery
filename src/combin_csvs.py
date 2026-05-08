8fd45d71128542d186782edc2d457813c3d7469c
shuseiyokoi <myoubann613@gmail.com>
2026-05-07 20:23:32 -0700

    modify code

--- src/combin_csvs.py
+++ src/combin_csvs.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 
 def combine_csv_files():
-    # Put your 4 CSV file paths here
     csv_files = [
         "../data/QCEW_2018_q1-q4_Butte_County.csv",
         "../data/QCEW_2018_q1-q4_Shasta_County.csv",
@@ -11,13 +10,10 @@ def combine_csv_files():
         "../data/QCEW_2019_q1-q4_Shasta_County.csv",
     ]
 
-    # Read and combine all CSVs
     dfs = [pd.read_csv(file) for file in csv_files]
 
-    # Combine rows because all files have the same columns
     combined_df = pd.concat(dfs, ignore_index=True)
 
-    # Save combined CSV
     combined_df.to_csv("../data/combined.csv", index=False)
 
     print("../data/Combined CSV saved as combined.csv")
